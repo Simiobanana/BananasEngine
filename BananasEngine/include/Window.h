d737aeda37bd44b42f4ab5a4abfdd780562012ff
Simiobanana <114266340+Simiobanana@users.noreply.github.com>
2024-01-22 10:48:28 -0600

    WindowClass2
    Me falto poner esto

--- BananasEngine/include/Window.h
+++ BananasEngine/include/Window.h
@@ -1,12 +1,23 @@
 #pragma once
 #include "Prerequisites.h"
 
+
+/// <summary>
+/// This class is in charge of generatting new window
+/// </summary>
 class Window
 {
 public:
 	Window();
 	~Window();
 
+	/// <summary>
+	/// The method is in charge of generate a window
+	/// </summary>
+	/// <param name="hInstance"> A single ID for the window / Copy of the resource </param>
+	/// <param name="nCmdShow"> Command list window </param>
+	/// <param name="wndproc"> Input Bridge </param>
+	/// <returns></returns>
 	HRESULT
 	init(HINSTANCE hInstance, int nCmdShow, WNDPROC wndproc);
 
